33f4199e98976cf9aa8d0c1cdf6a2c68dc3f3833
Mike <mikemcd.bbs@gmail.com>
2025-07-17 14:49:25 -0400

    Implement test tina component

---
+++ src/components/react/Test.tsx
@@ -0,0 +1,23 @@
+import type { HomeQuery, HomeQueryVariables } from "@tina/__generated__/types";
+import { useTina, tinaField } from "tinacms/dist/react";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+
+type Props = {
+  data: HomeQuery;
+  variables: HomeQueryVariables;
+  query: string;
+};
+
+export default function Test(props: Props) {
+  const { data } = useTina({
+    data: props.data,
+    variables: props.variables,
+    query: props.query,
+  });
+  const home = data.home;
+  return (
+    <div data-tina-field={tinaField(home, "body")}>
+      <TinaMarkdown content={home.body} />
+    </div>
+  );
+}
