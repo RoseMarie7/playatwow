7d25c850d104b139f8c412cf2aa07989e9e85a58
Mike <mikemcd.bbs@gmail.com>
2025-08-13 17:30:51 -0400

    Update astro config to support DNS on Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.playatwow.com",
   vite: {
     plugins: [tailwindcss()],
   },
