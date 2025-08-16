ad3408d00c59eaa68f3d7e356c53ce663cb362a8
Mike <mikemcd.bbs@gmail.com>
2025-08-15 23:50:48 -0400

    Add buttons linking to contact

--- tina/pages/AppointmentsSection.tsx
+++ tina/pages/AppointmentsSection.tsx
@@ -36,7 +36,7 @@ const components = {
 
 export default function AppointmentsSection(props: AppointmentsSectionProps) {
   return (
-    <Section className="bg-black">
+    <Section className="bg-black relative">
       <h2
         data-tina-field={tinaField(props, "title")}
         className="font-righteous uppercase text-3xl font-bold text-[#c49800] text-center"
@@ -62,24 +62,33 @@ export default function AppointmentsSection(props: AppointmentsSectionProps) {
               />
             )}
             <div className="flex flex-col justify-center">
-              <h3
-                data-tina-field={tinaField(block, "title")}
-                className="font-josefin-sans capitalize text-2xl font-bold text-white"
-              >
-                {block?.title}
-              </h3>
-              <div>
-                <p className="text-[#a4a4a4] pb-4">
-                  <span>
-                    <span data-tina-field={tinaField(block, "duration")}>
-                      {block?.duration}
+              <div className="grid grid-cols-1 sm:grid-cols-2 pb-4">
+                <h3
+                  data-tina-field={tinaField(block, "title")}
+                  className="font-josefin-sans capitalize text-2xl font-bold text-white"
+                >
+                  {block?.title}
+                </h3>
+                <a
+                  href="/contact"
+                  data-theme="light"
+                  className="btn mt-4 btn-block max-w-[11rem] sm:justify-self-end bg-[#c49800] text-white order-3 sm:order-none"
+                >
+                  Start Your Journey
+                </a>
+                <div>
+                  <p className="text-[#a4a4a4]">
+                    <span>
+                      <span data-tina-field={tinaField(block, "duration")}>
+                        {block?.duration}
+                      </span>
+                      <span> | </span>
+                      <span data-tina-field={tinaField(block, "cost")}>
+                        {block?.cost}
+                      </span>
                     </span>
-                    <span> | </span>
-                    <span data-tina-field={tinaField(block, "cost")}>
-                      {block?.cost}
-                    </span>
-                  </span>
-                </p>
+                  </p>
+                </div>
               </div>
               <div data-tina-field={tinaField(block, "content")}>
                 <TinaMarkdown
